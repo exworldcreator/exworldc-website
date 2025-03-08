5675c8ee87307ed5d90d59901adaf8f3c93df510
exworldcreator <rydanh400@gmail.com>
2025-03-08 15:13:04 +0300

    add education field

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,11 @@ const About = () => {
                     Main <br />
                     Hi, I'm Ian, a Web3 developer with over 2 years of experience in cryptocurrency 
                     and over 5 years of development experience in general. I first got into cryptocurrencies in 2022, 
-                    and just a year later I decided to switch and focus on programming in my favorite field.
+                    and just a year later I decided to switch and focus on programming in my favorite field. <br /> <br />
+
+                    Education <br />
+                    Saint Petersburg Electrotechnical University "LETI" <br />
+                    Bachelor's Degree in Information Systems and Technologies (2018–2022)
                 </p>
             </div>
         </div>
